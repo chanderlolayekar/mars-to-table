32c35477cc226b4416fb70aaf4e58f6d85a4e68d
chanderlolayekar <clolayekar@gmail.com>
2026-05-05 19:41:31 -0700

    Update crew.py

--- src/marstotable_biosim/crew.py
+++ src/marstotable_biosim/crew.py
@@ -1,1 +1,7 @@
+class Crew:
+    def __init__(self, config):
+        self.size = config['crew_size']
 
+    def consume(self, stores):
+        stores['calories'].remove(2800 * self.size)
+        stores['CO2'].add(1000 * self.size / 24)
