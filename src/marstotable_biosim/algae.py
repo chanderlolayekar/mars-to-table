c7472f4425106f4fe1e053e303bfaba5db681fb4
chanderlolayekar <clolayekar@gmail.com>
2026-05-05 19:43:10 -0700

    Update algae.py

--- src/marstotable_biosim/algae.py
+++ src/marstotable_biosim/algae.py
@@ -1,1 +1,11 @@
+class AlgaeBioreactor:
+    def __init__(self, config):
+        self.config = config
 
+    def calculate_avg_light(self, I0, X, kappa, L):
+        # Beer-Lambert placeholder
+        return I0
+
+    def tick(self, stores):
+        # Replace with your full equations
+        pass
