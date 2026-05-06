6c05300d9bb779eb53ba0c2c16dab8e8206b56c5
chanderlolayekar <clolayekar@gmail.com>
2026-05-05 19:44:23 -0700

    Update food_processor.py

--- src/marstotable_biosim/food_processor.py
+++ src/marstotable_biosim/food_processor.py
@@ -1,1 +1,8 @@
+class FoodProcessor:
+    def __init__(self, config):
+        self.config = config
 
+    def tick(self, stores):
+        if stores['edible_mycelium'].level > 5:
+            stores['calories'].add(2500)
+            stores['edible_mycelium'].remove(5)
