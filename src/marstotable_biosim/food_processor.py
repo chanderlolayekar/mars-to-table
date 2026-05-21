16f74700de8ca11751f18ac29c0d8d3a03b5789e
chanderlolayekar <clolayekar@gmail.com>
2026-05-21 08:30:54 -0700

    Update food_processor.py

--- src/marstotable_biosim/food_processor.py
+++ src/marstotable_biosim/food_processor.py
@@ -2,7 +2,7 @@ class FoodProcessor:
     def __init__(self, config):
         self.config = config
         self.calories_per_unit = config.get("calories_per_unit", 1000.0)
-        self.max_daily_conversion = config.get("max_daily_conversion", 1.0)
+        self.max_daily_conversion = config.get("max_daily_conversion", 5.0)
 
     def tick(self, stores):
         edible = stores["edible_mycelium"].level
